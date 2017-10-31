bfdbbdb536b27129b42ebf48c450d059bbe0f429
chenchong <congratulatecong@163.com>
2017-10-31 15:35:08 +0800

    [DEMAND]support lambda

--- app/src/main/java/com/cong/chenchong/activity/TreeViewerActivity.java
+++ app/src/main/java/com/cong/chenchong/activity/TreeViewerActivity.java
@@ -5,22 +5,17 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.cong.chenchong.R;
 import com.cong.chenchong.adapter.SimpleTreeListViewAdapter;
-import com.cong.chenchong.adapter.TreeListViewAdapter.OnTreeNodeClickListener;
 import com.cong.chenchong.bean.FileBean;
 import com.cong.chenchong.bean.OrgBean;
 import com.cong.chenchong.global.SlidingActivity;
-import com.cong.chenchong.util.Node;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -39,14 +34,15 @@ public class TreeViewerActivity extends SlidingActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tree_viewer);
 
-        TextView txtView = (TextView) findViewById(R.id.txt_title);
-        txtView.setText(getIntent().getStringExtra("title"));
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(getIntent().getStringExtra("title"));
+        toolbar.setNavigationOnClickListener(v -> onBackPressed());
 
         mListViewTreeViewer = (ListView) findViewById(R.id.listview_tree_viewer);
 
         initDatas();
         try {
-            mAdapter = new SimpleTreeListViewAdapter<OrgBean>(mListViewTreeViewer, this, mDatas2, 1);
+            mAdapter = new SimpleTreeListViewAdapter<>(mListViewTreeViewer, this, mDatas2, 1);
             mListViewTreeViewer.setAdapter(mAdapter);
         } catch (IllegalAccessException e) {
             e.printStackTrace();
@@ -56,39 +52,32 @@ public class TreeViewerActivity extends SlidingActivity {
     }
 
     private void initEvent() {
-        mAdapter.setOnTreeNodeClickListener(new OnTreeNodeClickListener() {
-            @Override
-            public void onClick(Node node, int position) {
-                if (node.isLeaf()) {
-                    Toast.makeText(TreeViewerActivity.this, node.getName(), Toast.LENGTH_SHORT).show();
-                }
+        mAdapter.setOnTreeNodeClickListener((node, position) -> {
+            if (node.isLeaf()) {
+                Toast.makeText(TreeViewerActivity.this, node.getName(), Toast.LENGTH_SHORT).show();
             }
         });
 
-        mListViewTreeViewer.setOnItemLongClickListener(new OnItemLongClickListener() {
+        mListViewTreeViewer.setOnItemLongClickListener((parent, view, position, id) -> {
+            // DialogFragment
+            final EditText et = new EditText(TreeViewerActivity.this);
+            new AlertDialog.Builder(TreeViewerActivity.this).setTitle("添加节点").setView(et).setPositiveButton("确定", new OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
 
-            @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
-                // DialogFragment
-                final EditText et = new EditText(TreeViewerActivity.this);
-                new AlertDialog.Builder(TreeViewerActivity.this).setTitle("添加节点").setView(et).setPositiveButton("确定", new OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-
-                        if (TextUtils.isEmpty(et.getText().toString())) {
-                            return;
-                        }
-                        mAdapter.addExtraNode(position, et.getText().toString());
+                    if (TextUtils.isEmpty(et.getText().toString())) {
+                        return;
                     }
-                }).setNegativeButton("取消", null).show();
+                    mAdapter.addExtraNode(position, et.getText().toString());
+                }
+            }).setNegativeButton("取消", null).show();
 
-                return true;
-            }
+            return true;
         });
     }
 
     private void initDatas() {
-        mDatas = new ArrayList<FileBean>();
+        mDatas = new ArrayList<>();
         FileBean bean = new FileBean(1, 0, "根目录1");
         mDatas.add(bean);
         bean = new FileBean(2, 0, "根目录2");
@@ -107,7 +96,7 @@ public class TreeViewerActivity extends SlidingActivity {
         mDatas.add(bean);
 
         // initDatas
-        mDatas2 = new ArrayList<OrgBean>();
+        mDatas2 = new ArrayList<>();
         OrgBean bean2 = new OrgBean(1, 0, "根目录1");
         mDatas2.add(bean2);
         bean2 = new OrgBean(2, 0, "根目录2");
